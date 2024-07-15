9759829690878d1e16a31947bdcb04511ff71e2d
Katrina <97328087+k-trina@users.noreply.github.com>
2024-07-15 18:02:40 -0400

    Update process_login.php

--- scripts/process_login.php
+++ scripts/process_login.php
@@ -9,17 +9,17 @@
 
 <body>
     <?php
-    //get user input
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    //check if username and password are valid
-    if ($password!= "password" || $username!= "username") { //change this to whatever we want to username and password to be
-        echo "Invalid username/password. Please try again.";
-        exit();
-    };
-    //successful login, set session variable
-    session_start();
-    $_SESSION["user"] = $username;
+        //get user input
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        //check if username and password are valid
+        if ($password!= "password" || $username!= "username") { //change this to whatever we want to username and password to be
+            echo "Invalid username/password. Please try again.";
+            exit();
+        };
+        //successful login, set session variable
+        session_start();
+        $_SESSION["user"] = $username;
     ?>
 </body>
 
