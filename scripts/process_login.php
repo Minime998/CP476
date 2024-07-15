03670238a4686817902ea2fe8741f44637b79f12
Katrina <97328087+k-trina@users.noreply.github.com>
2024-07-15 17:51:33 -0400

    Update process_login.php
    added login functionality

--- scripts/process_login.php
+++ scripts/process_login.php
@@ -4,15 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Page</title>
 </head>
 
 <body>
     <?php
+    //get user input
     $username = $_POST['username'];
     $password = $_POST['password'];
-    print "$username";
-    echo "$password";
+    //check if username and password are valid
+    if ($password!= "password" || $username!= "username") { //change this to whatever we want to username and password to be
+        echo "Invalid username/password. Please try again.";
+        exit();
+    };
+    //successful login, set session variable
+    session_start();
+    $_SESSION["user"] = $username;
     ?>
 </body>
 
